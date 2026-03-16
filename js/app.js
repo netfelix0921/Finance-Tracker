e992efcf6de7ff14dc899fd443ce32b8c01bcae5
netfelix0921 <alforquefelixmarco@gmail.com>
2026-03-17 01:26:46 +0800

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+import { calculateNetWorth } from "./finance-engine.js"
+import { loadData } from "./storage-engine.js"
 
+function init(){
+
+const accounts = loadData("accounts")
+
+const netWorth = calculateNetWorth(accounts)
+
+console.log("Net Worth:", netWorth)
+
+}
+
+document.addEventListener("DOMContentLoaded", init)
